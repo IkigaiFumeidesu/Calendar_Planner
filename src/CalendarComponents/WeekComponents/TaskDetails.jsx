4ea0d3153a556bd5a077f0c1bc83ac26c624c2c0
IkigaiFumeidesu <hnzsch007@gmail.com>
2024-06-03 17:03:02 +0200

    exported deleteTask function into a new file

--- src/CalendarComponents/WeekComponents/TaskDetails.jsx
+++ src/CalendarComponents/WeekComponents/TaskDetails.jsx
@@ -1,32 +1,15 @@
 import React from 'react';
-
+import deleteTask from './deleteTask';
 function TaskDetails(props) {
 
-    const deleteTask = () => {
-
-        // Removing clicked task by using the index passed in props
-        const existingTasksArray = JSON.parse(localStorage.getItem("Tasks"));
-        existingTasksArray.splice(props.makeAnotherPlan[1], 1);
-        localStorage.setItem("Tasks", JSON.stringify(existingTasksArray));
-
-        // Same as above, but no convertion to JSON needed
-        const existingDatesArray = localStorage.getItem("Date").split("_");
-        existingDatesArray.splice(props.makeAnotherPlan[1], 1);
-        localStorage.setItem("Date", existingDatesArray.join("_"));
-
-        // For every 1 element in existingTasksArray there are 2 in existingTaskDetailsArray - hence (say props is 0), 0 * 2 = 0, and 0 * 2 + 1 = 1, so splice(0,1)
-        const existingTaskDetailsArray = JSON.parse(localStorage.getItem("TaskDetails"));
-        existingTaskDetailsArray.splice(props.makeAnotherPlan[1] * 2, 2);
-        localStorage.setItem("TaskDetails", JSON.stringify(existingTaskDetailsArray));
-    }
-    const temp = props.makeAnotherPlan[0].date.split("-");
+    const HourToAndFrom = props.makeAnotherPlan[0].date.split("-");
     return (
         <>
             <div className="task-details_background" onClick={() => { props.setTaskDetails(false) }}></div>
             <div className="task-details_div">
                 <button className="task-details_cancel" onClick={() => { props.setTaskDetails(false) }}>X</button>
                 <div className="task-details_header">
-                    <h1>{temp[2] + "." + temp[1]}</h1>
+                    <h1>{HourToAndFrom[2] + "." + HourToAndFrom[1]}</h1>
                     <h3>{props.makeAnotherPlan[0].title}</h3>
                 </div>
                 <div className="task-details_header">
@@ -40,7 +23,7 @@ function TaskDetails(props) {
                 <hr />
 
                 <div className="task-details_bottom">
-                    <button className="task-details_buttons" onClick={() => { deleteTask(); props.setTaskDetails(false) }}>Delete this task!</button>
+                    <button className="task-details_buttons" onClick={() => { deleteTask(props.makeAnotherPlan); props.setTaskDetails(false) }}>Delete this task!</button>
                     <button className="task-details_buttons" onClick={() => { props.setTaskDetails(false) }}>Hide the details!</button>
                 </div>
             </div>
---
+++ src/CalendarComponents/WeekComponents/deleteTask.jsx
@@ -0,0 +1,19 @@
+
+function deleteTask() {
+
+    // Removing clicked task by using the index passed in props
+    const existingTasksArray = JSON.parse(localStorage.getItem("Tasks"));
+    existingTasksArray.splice(props.makeAnotherPlan[1], 1);
+    localStorage.setItem("Tasks", JSON.stringify(existingTasksArray));
+
+    // Same as above, but no convertion to JSON needed
+    const existingDatesArray = localStorage.getItem("Date").split("_");
+    existingDatesArray.splice(props.makeAnotherPlan[1], 1);
+    localStorage.setItem("Date", existingDatesArray.join("_"));
+
+    // For every 1 element in existingTasksArray there are 2 in existingTaskDetailsArray - hence (say props is 0), 0 * 2 = 0, and 0 * 2 + 1 = 1, so splice(0,1)
+    const existingTaskDetailsArray = JSON.parse(localStorage.getItem("TaskDetails"));
+    existingTaskDetailsArray.splice(props.makeAnotherPlan[1] * 2, 2);
+    localStorage.setItem("TaskDetails", JSON.stringify(existingTaskDetailsArray));
+}
+export default deleteTask;
