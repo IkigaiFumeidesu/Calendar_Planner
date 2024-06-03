73089c646d4de2b0b1c63fb859d90a88a7c06686
IkigaiFumeidesu <hnzsch007@gmail.com>
2024-06-03 21:25:35 +0200

    moved setTask function from AddTask to a new file

--- src/CalendarComponents/AddTask.jsx
+++ src/CalendarComponents/AddTask.jsx
@@ -1,5 +1,5 @@
 ﻿import { useState, useRef, useEffect } from "react";
-
+import setTask from "./setTask";
 function AddTask(props) {
 
     // Clicked cell return a key X:00-XXXX-??-??
@@ -197,27 +197,3 @@ function AddTask(props) {
     )
 }
 export default AddTask
-
-
-function setTask(objectForm) {
-
-    localStorage.setItem("Date", localStorage.getItem("Date") + objectForm.date + "_");
-
-    // Converting Task Array consisting of objects into an array and storing it locally
-    if (localStorage.getItem("Tasks") === "") {
-        localStorage.setItem("Tasks", JSON.stringify([objectForm]));
-    } else {
-        const addObjectIntoStorage = JSON.parse(localStorage.getItem("Tasks"));
-        addObjectIntoStorage.push(objectForm);
-        localStorage.setItem("Tasks", JSON.stringify(addObjectIntoStorage));
-    }
-    // Converting properties of an object into an array and storing it locally
-    if (localStorage.getItem("TaskDetails") === "") {
-        localStorage.setItem("TaskDetails", JSON.stringify([objectForm.title, objectForm.description]));
-    } else {
-        const addObjectPropsIntoStorage = JSON.parse(localStorage.getItem("TaskDetails"));
-        addObjectPropsIntoStorage.push(objectForm.title);
-        addObjectPropsIntoStorage.push(objectForm.description);
-        localStorage.setItem("TaskDetails", JSON.stringify(addObjectPropsIntoStorage));
-    }
-}
