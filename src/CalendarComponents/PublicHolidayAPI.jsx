ede8a791eaa477a6b7f3cf9e7d92f5cd3fcd9b5b
IkigaiFumeidesu <hnzsch007@gmail.com>
2024-06-03 20:22:30 +0200

    cleaned up return and import in the PublicHolidayAPI

--- src/CalendarComponents/PublicHolidayAPI.jsx
+++ src/CalendarComponents/PublicHolidayAPI.jsx
@@ -1,31 +1,25 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 
 function PublicHolidayAPI(props) {
 
-        // Component to get public holiday API based on the country origin which was obtained at App.jsx component
-        useEffect(() => {
-            fetch(`https://date.nager.at/api/v3/publicholidays/${props.initialYearNumber}/${props.initialCountry}`)
-                .then(res => res.json())
-                .then(
-                    (result) => {
-                        props.setPublicHoliday(result.map((item) => {
+    // Component to get public holiday API based on the country origin which was obtained at App.jsx component
+    useEffect(() => {
+        fetch(`https://date.nager.at/api/v3/publicholidays/${props.initialYearNumber}/${props.initialCountry}`)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    props.setPublicHoliday(result.map((item) => {
 
-                            // Setting the format from the API to YYYY-MM-DD without 0 before the month or date
-                            item.date[8] === "0" && (item.date = item.date.slice(0, 8) + item.date.slice(9));
-                            item.date[5] === "0" && (item.date = item.date.slice(0, 5) + item.date.slice(6));
-                            return [item.date, item.name]
-                        }));
-                    },
-                    (error) => {
-                        alert("Public holidays cannot be reached.")
-                    }
-                )
-        }, []);
-
-    return (
-        <>
-        </>
-  );
+                        // Setting the format from the API to YYYY-MM-DD without 0 before the month or date
+                        item.date[8] === "0" && (item.date = item.date.slice(0, 8) + item.date.slice(9));
+                        item.date[5] === "0" && (item.date = item.date.slice(0, 5) + item.date.slice(6));
+                        return [item.date, item.name]
+                    }));
+                },
+                (error) => {
+                    alert("Public holidays cannot be reached.")
+                }
+            )
+    }, []);
 }
-
 export default PublicHolidayAPI;
